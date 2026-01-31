ccab14950b67f707b8903fd07a6810bee0ca304f
dd <xeon3883@gmail.com>
2026-01-31 12:58:24 +0300

    upd projects

--- src/app/api/featuredproperty.tsx
+++ src/app/api/featuredproperty.tsx
@@ -1,21 +1,16 @@
 import { FeaturedProperty } from '@/types/featuredProperty'
 
 export const featuredProprty: FeaturedProperty[] = [
-  {
-    scr: '/images/photo.jpg',
-    alt: 'property6',
-  },
-  {
-    scr: '/images/photo.jpg',
-    alt: 'property7',
-  },
-  {
-    scr: '/images/photo.jpg',
-    alt: 'property8',
-  },
-  {
-    scr: '/images/photo.jpg',
-    alt: 'property9',
-  },
+	{
+		scr: '/images/photo.jpg',
+		alt: 'property6',
+	},
+	{
+		scr: '/images/photo.jpg',
+		alt: 'property7',
+	},
+	{
+		scr: '/images/photo.jpg',
+		alt: 'property8',
+	},
 ]
-
